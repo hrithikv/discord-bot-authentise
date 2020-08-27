ce2cbcdf74c3aef78b973ab7431d7c2e0053c407
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-27 17:08:33 -0400

    Global variables for message and connection. play and search working

---
+++ helper/youtubeHelper.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const apiKey = process.env.YOUTUBE_DATA_API_KEY_AUTHENTISE;
+
+module.exports.searchByQueries = (queries, callback, message) => {
+	message.channel.send(`**Searching songs ... :mag_right:**`);
+	axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${queries}&maxResults=5&key=${apiKey}`, {
+    }).then(res => {
+    	if (res.data) {
+	    	callback(res.data);
+    	}
+    }).catch((err) => {
+    	console.log(err)
+    })
+}
